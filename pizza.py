656410082082061d6a4bb4763989fb307c039d27
kahenozturk <kahenozturk@gmail.com>
2023-03-08 13:52:47 +0100

    feat : created Pizza subclass

--- pizza.py
+++ pizza.py
@@ -8,3 +8,27 @@ class Pizza:
 
     def get_cost(self):
         return self.cost
+    
+class Classic(Pizza):
+    def __init__(self):
+        super().__init__()
+        self.description = "Classic Pizza"
+        self.cost = 8.99
+        
+class Margherita(Pizza):
+    def __init__(self):
+        super().__init__()
+        self.description = "Margherita Pizza"
+        self.cost = 9.99
+
+class TurkPizza(Pizza):
+    def __init__(self):
+        super().__init__()
+        self.description = "Turk Pizza"
+        self.cost = 11.99
+
+class PlainPizza(Pizza):
+    def __init__(self):
+        super().__init__()
+        self.description = "Plain Pizza"
+        self.cost = 7.99
