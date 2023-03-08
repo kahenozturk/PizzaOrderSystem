552a1188af654ee9aa8db8a4426c16e3f0f96608
kahenozturk <kahenozturk@gmail.com>
2023-03-08 13:51:58 +0100

    feat : created Pizza superclass

---
+++ pizza.py
@@ -0,0 +1,10 @@
+class Pizza:
+    def __init__(self):
+        self.description = "Unknown"
+        self.cost = 0.0
+
+    def get_description(self):
+        return self.description
+
+    def get_cost(self):
+        return self.cost
