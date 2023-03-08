022fc317cbae53d3863babf60d736b5e6274b80f
kahenozturk <kahenozturk@gmail.com>
2023-03-08 13:54:42 +0100

    feat : created Decorator superclass and  subclass'

--- pizza.py
+++ pizza.py
@@ -32,3 +32,76 @@ class PlainPizza(Pizza):
         super().__init__()
         self.description = "Plain Pizza"
         self.cost = 7.99
+
+class Decorator(Pizza):
+    def __init__(self, component):
+        self.component = component
+
+    def get_cost(self):
+        return self.component.get_cost() + self.cost
+
+    def get_description(self):
+        return self.component.get_description() + ", " + self.description
+    
+class Olives(Decorator):
+    def __init__(self, component):
+        super().__init__(component)
+        self.description = "Olives sauce"
+        self.cost = 1.50
+        
+    def get_name(self):
+        return self.description
+
+class Mushrooms(Decorator):
+    def __init__(self, component):
+        super().__init__(component)
+        self.description = "Mushrooms sauce"
+        self.cost = 1.00
+        
+    def get_name(self):
+        return self.description
+    
+class GoatCheese(Decorator):
+    def __init__(self, component):
+        super().__init__(component)
+        self.description = "Goat Cheese sauce" 
+        self.cost = 2.00
+        
+    def get_name(self):
+        return self.description
+
+class Meat(Decorator):
+    def __init__(self, component):
+        super().__init__(component)
+        self.description = "Meat sauce"
+        self.cost = 2.50
+        
+    def get_name(self):
+        return self.description
+
+class Onions(Decorator):
+    def __init__(self, component):
+        super().__init__(component)
+        self.description = "Onions sauce"
+        self.cost = 1.00
+        
+    def get_name(self):
+        return self.description
+
+class Corn(Decorator):
+    def __init__(self, component):
+        super().__init__(component)
+        self.description = "Corn sauce"
+        self.cost = 1.50
+        
+    def get_name(self):
+        return self.description
+
+class Plain(Decorator):
+    def __init__(self, component):
+        super().__init__(component)
+        self.description = "no sauce"
+        self.cost = 0.0
+        
+    def get_name(self):
+        return self.description
